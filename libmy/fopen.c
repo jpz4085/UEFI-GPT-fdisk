7940d64ee1c098ea7957a56c384705e9c5c4176c
jpz4085 <jpz408@yahoo.com>
2024-10-06 17:26:13 -0400

    Update author information in previously modified files.

--- libmy/fopen.c
+++ libmy/fopen.c
@@ -5,8 +5,8 @@
  * UEFI GPT fdisk est un portage de GPT fdisk vers UEFI/BIOS.
  * Ce fichier a été initié par Bernard Burette en février 2014.
  *
- * All this work is copyleft Bernard Burette.
- * Gauche d'auteur Bernard Burette.
+ * Original work is copyleft Bernard Burette.
+ * Modifications are copyleft Joseph Zeller.
  *
  * This program is distributed under the terms of the GNU GPL version 2.
  * La diffusion de ce code est faite selon les termes de la GPL GNU version 2.
--- libmy/strxfrm.c
+++ libmy/strxfrm.c
@@ -7,8 +7,8 @@
  * Ce fichier est récupéré soit de "GNU libc" soit de "dietlibc"
  * soit encore il a été créé de toutes pièces.
  *
- * All this work is copyleft Bernard Burette.
- * Gauche d'auteur Bernard Burette.
+ * Original work is copyleft Bernard Burette.
+ * Modifications are copyleft Joseph Zeller.
  *
  * This program is distributed under the terms of the GNU GPL version 2.
  * La diffusion de ce code est faite selon les termes de la GPL GNU version 2.
